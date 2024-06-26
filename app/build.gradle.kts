4bccd8de5a1a7bd0fd65b2c81c052adbf85859ee
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-06-26 10:02:05 +0530

    Ui Designed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.lottie)
+    implementation(libs.converter.gson)
+    implementation(libs.retrofit)
 }
