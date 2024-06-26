d86de2610aea38fc66331b4c38804733c2a1d470
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-06-26 13:44:13 +0530

    retrofit api and core function added

--- app/src/main/java/com/gyarsilalsolanki011/weathercap/MainActivity.kt
+++ app/src/main/java/com/gyarsilalsolanki011/weathercap/MainActivity.kt
@@ -1,13 +1,142 @@
 package com.gyarsilalsolanki011.weathercap
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.gyarsilalsolanki011.weathercap.databinding.ActivityMainBinding
+import com.gyarsilalsolanki011.weathercap.dataclasses.WeatherCap
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
+// AppId:↘️d4b565e178d6c7d57dca471270563602
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        fetchWeatherData("khandwa")
+        searchCity()
     }
+
+    private fun searchCity() {
+        val searchView = binding.searchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    fetchWeatherData(query)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+
+        })
+    }
+
+    private fun fetchWeatherData(cityName : String) {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .build().create(ApiInterface::class.java)
+        val response = retrofit.getWeatherData(cityName, "d4b565e178d6c7d57dca471270563602", "metric")
+        response.enqueue(object : Callback<WeatherCap>{
+            @SuppressLint("SetTextI18n")
+            override fun onResponse(p0: Call<WeatherCap>, p1: Response<WeatherCap>) {
+                val responseBody = p1.body()
+                if (p1.isSuccessful && responseBody != null){
+                    val temperature = responseBody.main.temp.toString()
+                    val humidity = responseBody.main.humidity
+                    val windSpeed = responseBody.wind.speed
+                    val sunrise = responseBody.sys.sunrise.toLong()
+                    val sunset = responseBody.sys.sunset.toLong()
+                    val seaLevel = responseBody.main.pressure
+                    val condition = responseBody.weather.firstOrNull()?.main?: "unknown"
+                    val maxTemp = responseBody.main.temp_max
+                    val minTemp = responseBody.main.temp_min
+
+                    binding.weather.text = condition
+                    binding.temp.text = "$temperature °C"
+                    binding.max.text = "Max Temp : $maxTemp °C"
+                    binding.min.text = "Min Temp : $minTemp °C"
+                    binding.humidity.text = "$humidity %"
+                    binding.windSpeed.text = "$windSpeed m/s"
+                    binding.sunrise.text = time(sunrise)
+                    binding.sunset.text = time(sunset)
+                    binding.sea.text = "$seaLevel hPa"
+                    binding.condition.text = condition
+                    binding.day.text = dayName(System.currentTimeMillis())
+                    binding.date.text = date()
+                    binding.cityName.text = cityName
+                    //Log.d("TAG", "Temperature : $temperature")
+
+                    changAnimationsAccordingToWeather(condition)
+                }
+            }
+
+            override fun onFailure(p0: Call<WeatherCap>, p1: Throwable) {
+
+            }
+
+        })
+    }
+
+    private fun changAnimationsAccordingToWeather(condition: String) {
+        when (condition){
+            "Clear Sky", "Sunny", "Clear" ->{
+                binding.root.setBackgroundResource(R.drawable.sunny_background)
+                binding.lottieAnimationView.setAnimation(R.raw.sun)
+            }
+
+            "Partly Clouds", "Clouds", "Overcast", "Mist", "Foggy" ->{
+                binding.root.setBackgroundResource(R.drawable.cloud_background)
+                binding.lottieAnimationView.setAnimation(R.raw.cloud)
+            }
+
+            "Light Rain", "Drizzle", "Moderate Rain", "Showers", "Heavy Rain" ->{
+                binding.root.setBackgroundResource(R.drawable.rain_background)
+                binding.lottieAnimationView.setAnimation(R.raw.rain)
+            }
+
+            "Light Snow", "Moderate Snow", "Heavy Snow", "Blizzard" ->{
+                binding.root.setBackgroundResource(R.drawable.snow_background)
+                binding.lottieAnimationView.setAnimation(R.raw.snow)
+            }
+            else ->{
+                binding.root.setBackgroundResource(R.drawable.sunny_background)
+                binding.lottieAnimationView.setAnimation(R.raw.sun)
+            }
+        }
+        binding.lottieAnimationView.playAnimation()
+    }
+
+    private fun date(): String {
+        val sdf = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+        return sdf.format((Date()))
+    }
+
+    fun dayName(timestamp: Long): String{
+        val sdf = SimpleDateFormat("EEEE", Locale.getDefault())
+        return sdf.format((Date()))
+    }
+
+    fun time(timestamp: Long): String{
+        val sdf = SimpleDateFormat("HH:mm", Locale.getDefault())
+        return sdf.format((Date(timestamp*1000)))
+    }
+
 }
